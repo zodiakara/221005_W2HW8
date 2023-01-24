3bceda8167b84f6beb613f2ef03f6f87ada25bc1
zodiakara <114649190+zodiakara@users.noreply.github.com>
2023-01-24 19:49:20 +0100

    hw update

--- hw2.js
+++ hw2.js
@@ -1,5 +1,3 @@
-// Additional assignments for Day 8
-
 /* EXTRA 1
  Write a piece of code for reverting an array.
  es:
@@ -14,41 +12,48 @@ console.log(arr2);
  Write a piece of code for getting the maximum numerical value from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const arrOfNumbers = [4, 6, 7, 34, 1];
+const maxNumber = Math.max(...arrOfNumbers);
+console.log(maxNumber);
 
 /* EXTRA 3
  Write a piece of code for getting the minimum numerical value from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const minNumber = Math.min(...arrOfNumbers);
+console.log(minNumber);
 
 /* EXTRA 4
  Write a piece of code for getting only even numerical values from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const onlyEven = arrOfNumbers.filter((x) => x % 2 === 0);
+console.log("this is a filtered array with even numbers", onlyEven);
 
 /* EXTRA 5
  Write a piece of code for deleting only even entries from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
-
+const onlyOdd = arrOfNumbers.filter((x) => x % 2 !== 0);
+console.log("this is a filtered array with odd numbers", onlyOdd);
 /* EXTRA 6
  Write a piece of code for removing all the vowels from a string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let strToRemoveVowels = "this is a simple string";
+let removed = strToRemoveVowels.replace(/[aeiouye ]/gi, "");
+console.log(removed);
 
 /* EXTRA 7
  Write a piece of code for increasing all the numerical values in a array by 1.
 */
 
-/* WRITE YOUR ANSWER HERE */
-
+let arrPlusOne = arr1.map((x) => x + 1);
+console.log(arrPlusOne);
 /* EXTRA 8 
  Replace all the strings contained in an array with their length.
  es.: ["strive", "is", "great"] => [6, 2, 5]
 */
-
-/* WRITE YOUR ANSWER HERE */
+let arrayOfWords = ["this", "is", "an", "array", "of", "words"];
+let arrayofLengths = arrayOfWords.map((word) => word.length);
+console.log("this is an array of lengths:", arrayofLengths);
